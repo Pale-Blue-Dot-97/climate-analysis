48dfb1d68d47b07708456f4548681a54684a4919
Harry Baker <hjbaker97@gmail.com>
2023-11-15 12:55:54 +0000

    Added docstrings to `temp_conversion.py`

--- temp_conversion.py
+++ temp_conversion.py
@@ -1,6 +1,22 @@
+"""
+A module for converting temperature from imperial to metric units
+Will throw ValueErrors for temperature < absolute zero
+
+Functions:
+    convert_fahr_to_celsius: convert Fahrenheit to Celsius
+    convert_fahr_to_kelvin: converts Fahrenheit to Kelvin
+"""
+
 # Todo: Code is a bit unclear
 
 def convert_fahr_to_celsius(fahr):
+    """
+    Given a temperature in Fahrenheit, converts it to Celsius
+
+    :param fahr: the temperature in Fahrenheit
+    :raises ValueError: if the temperature is below absolute zero
+    :return: the temperature in Celsius
+    """
     celsius = (fahr - 32) * (5 / 9)
     if celsius < -273.15:
         # If temperature is below absolute zero. throw an error.
