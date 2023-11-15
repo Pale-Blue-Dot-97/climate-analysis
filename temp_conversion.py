179560380e86f05d2e3bb735b260feae9528392c
Harry Baker <hjbaker97@gmail.com>
2023-11-15 12:49:20 +0000

    Added comment explaining <absolute zero check

--- temp_conversion.py
+++ temp_conversion.py
@@ -3,6 +3,7 @@
 def convert_fahr_to_celsius(fahr):
     celsius = (fahr - 32) * (5 / 9)
     if celsius < -273.15:
+        # If temperature is below absolute zero. throw an error.
         raise ValueError(
             f"Trying to convert impossible temperature: {fahr}F"
         )
