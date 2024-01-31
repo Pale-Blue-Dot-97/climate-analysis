d7109506ac709ba8935497159d0222b850ca006b
Harry Baker <hjbaker97@gmail.com>
2024-01-31 15:34:36 +0000

    Rename variables and document `temp_conversion.py`

--- temp_conversion.py
+++ temp_conversion.py
@@ -1,10 +1,29 @@
-# Todo: Code is a bit unclear
+"""Module containing functions to convert temperatures between scales."""
 
-def Fc(x):
-    Y = (x - 32) * (5 / 9)
-    return Y
 
-def FK(x):
-    y = Fc(x)
-    z = y + 273.15
-    return z
+def convert_fahr_to_celcius(fahr):
+    """
+    Converts Fahrenheit to Celcius.
+    
+    Args:
+        fahr: Temperature in Fahrenheit.
+
+    Returns:
+        Temperature in Celcius.
+    """
+    celcius = (fahr - 32) * (5 / 9)
+    return celcius
+
+def convert_fahr_to_kelvin(fahr):
+    """
+    Converts Fahrenheit to Kelvin.
+
+    Args:
+        fahr: Temperature in Fahrenheit
+
+    Returns:
+        Temperature in Kelvin.
+    """
+    fahr = convert_fahr_to_celcius(x)
+    kelvin = fahr + 273.15
+    return kelvin
