bbf867bc4ed015ba245bf822bd579cc49200f32b
Harry Baker <hjbaker97@gmail.com>
2024-11-28 15:03:00 +0000

    Update rainfall_conversion.py

--- rainfall_conversion.py
+++ rainfall_conversion.py
@@ -1,3 +1,11 @@
+"""Functions for converting imperial units to metric"""
+
 def inches_to_mm(inches):
+	"""
+ 	Converts rainfall in inches to mm
+
+   	Args:
+    		inches (float):
+        """
 	mm = inches * 25.4
 	return mm
